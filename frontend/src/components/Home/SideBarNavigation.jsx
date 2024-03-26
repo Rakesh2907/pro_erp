ef48121aaeabf68ff79de42d4ab97def3a5753fa
padmakar <rakeshahirrao29@gmail.com>
2024-03-26 18:32:12 +0530

    dynamic sub menu in left siderbar navigation with unique parent menu id

--- frontend/src/components/Home/SideBarNavigation.jsx
+++ frontend/src/components/Home/SideBarNavigation.jsx
@@ -44,7 +44,7 @@ const SiderBarNavigation = ({ onOpen, loadedMenus }) => {
               <NavLink
                 to={`/${menuComponent.url}`}
                 className={({ isActive }) =>
-                  'group relative flex items-center gap-2.5 rounded-md px-3 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
+                  'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
                   (isActive && '!text-white')
                 }
               >
