4e76b0cd0a0e9a14830e2a6fa85f84332ecb2fa1
padmakar <rakeshahirrao29@gmail.com>
2024-03-30 19:38:59 +0530

    post blogs

--- frontend/src/components/Home/Dashboard.jsx
+++ frontend/src/components/Home/Dashboard.jsx
@@ -22,7 +22,7 @@ const Dashboard = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4">
        {loadedModules.map((moduleComponent, index) => (
-                <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" key={index}>
+                <div className="max-w-sm p-6 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" key={index}>
                     <div className="flex">
                     <img src={`/images/icons/${moduleComponent.module_icon}`} alt="Module Icon" className="w-10 h-10 mr-4" />
                         
