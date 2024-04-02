ea47e386a8d09fd2f9d2df33f87a2e4f87b3f913
padmakar <rakeshahirrao29@gmail.com>
2024-04-02 20:18:54 +0530

    post blog

--- backend/model/mymodule.js
+++ backend/model/mymodule.js
@@ -2,7 +2,7 @@ const db = require("../db/mysql_connection_pool");
 
 async function getModuleDetails() {
     try {
-        const [rows, fields] = await db.query('SELECT * FROM pro_modules');
+        const [rows, fields] = await db.query('SELECT * FROM pro_modules WHERE is_deleted = ?',['0']);
         return rows;
     } catch (error) {
         console.error('Error fetching users:', error);
--- frontend/src/pages/TimeLinePage.jsx
+++ frontend/src/pages/TimeLinePage.jsx
@@ -4,16 +4,17 @@ import HomePage from './HomePage';
 const TimeLinePage = () => {
   return (
     <div>
-       <HomePage> 
+      <HomePage>
         <div className="table table-fixed border-spacing-0 w-full">     {/* <!-- box --> */}
           <div className="table-cell align-top h-full float-none overflow-x-hidden"> {/* <!-- box-content --> */}
-
-              <form className="block mt-0 isolate-unicode general-form">
-                <div className="table border-spacing-0 w-full"> {/* <!-- box --> */}
-                  <div className="align-top h-full float-none overflow-x-hidden w-20 pr-4 !important table-cell !important">
-                      <img src="images/user/user-01.png" alt="..."  className="h-auto max-w-full rounded-full"/>
-                  </div> 
-                  <div className="table-cell align-top h-full float-none overflow-x-hidden bg-white mb-15 rounded-5">
+          <div id="timeline-content" class="p-4 clearfix mb-5 overflow-hidden !important overflow-anchor-none touch-action-auto " style={{position: 'relative'}}>
+              <div id="post-form-container">
+                <form className="block mt-0 isolate-unicode mb-10">
+                  <div className="table border-spacing-0 w-full"> {/* <!-- box --> */}
+                    <div className="align-top h-full float-none overflow-x-hidden w-20 pr-4 !important table-cell !important">
+                      <img src="images/user/user-01.png" alt="..." className="h-auto max-w-full rounded-full" />
+                    </div>
+                    <div className="table-cell align-top h-full float-none overflow-x-hidden bg-white mb-15 rounded-5">
                       <textarea rows="3" placeholder="Share idea or documents" className="w-full border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"></textarea>
                       <footer className="rounded-tl rounded-tr border-gray-300 bg-gray-500 !important clear-both">
                         <button className="text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 me-2 mb-2 float-left">
@@ -24,16 +25,45 @@ const TimeLinePage = () => {
                           Upload File
                         </button>
                         <button className="text-white bg-[#2557D6] hover:bg-[#2557D6]/90 focus:ring-4 focus:ring-[#2557D6]/50 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#2557D6]/50 me-2 mb-2 float-right" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-send icon-16"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg> Post</button>
-                    </footer>
-                  </div> 
+                      </footer>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div id="post_timeline" className="list-none w-full min-h-full mx-auto relative">
+                <div id="post-content-container-6" className="post-content">
+                  <div className="clearfix">
+                    <div className="clearfix post-date">
+                        <span className='bg-blue-500 text-white py-2 px-5 pb-2 rounded-lg'>30/03/2024</span>
+                    </div>
+                    <div className="mb-5 clearfix transition-all duration-1000 my-4 relative flex flex-col min-w-0 overflow-wrap-normal bg-white bg-clip-border border border-solid border-black border-opacity-25 rounded">  {/* card */}
+                        <div className="p-15"> {/** Card-body */} 
+
+                        </div>
+                    </div>
+                  </div>
                 </div>
-              </form>  
+                <div className="post-content">
+                  <div className="clearfix">
+                    <div className="post-date clearfix">
+                      <span className='bg-blue-500 text-white py-2 px-5 pb-2 rounded-lg'>30/03/2024</span>
+                    </div>
+                    <div className="mb-5 clearfix transition-all duration-1000 my-4 relative flex flex-col min-w-0 overflow-wrap-normal bg-white bg-clip-border border border-solid border-black border-opacity-25 rounded">  {/* card */}
+                        <div className="p-15"> {/** Card-body */} 
+
+                        </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+           </div>   
           </div>
           {/*<div style={{width: '250px', minHeight: '100%'}} className="table-cell align-top h-full float-none overflow-x-hidden bg-white">
-             dsd sda dasd
+             User List
   </div>*/}
-        </div>  
-       </HomePage>
+        </div>
+
+      </HomePage>
     </div>
   )
 }
