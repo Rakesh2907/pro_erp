44ab6365c60cb9027e5de27a9baf763ab1672284
padmakar <rakeshahirrao29@gmail.com>
2024-06-22 18:01:31 +0530

    inventory related pages

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -2,20 +2,10 @@ import SignInPage from "../pages/SignInPage";
 import SignUpPage from "../pages/SignUpPage";
 import ActivationPage from "../pages/ActivationPage";
 import HomePage from "../pages/HomePage";
-import ProjectManagementPage from "../pages/ProjectManagementPage";
-import HRMPage from "../pages/HumanResourceManagementPage";
-import InventoryManagementPage from "../pages/InventoryManagementPage";
-import EmploymentPage from "../pages/EmploymentPage";
-import CRMPage from "../pages/CRMPage";
 
 export {
     SignInPage,
     SignUpPage,
     ActivationPage,
-    HomePage,
-    ProjectManagementPage,
-    HRMPage,
-    InventoryManagementPage,
-    EmploymentPage,
-    CRMPage
+    HomePage
 }
